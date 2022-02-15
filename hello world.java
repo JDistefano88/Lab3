2a07465c42c46cfa54b8ec53dd784ff08d151fdd
sarmiene <82686188+sarmiene@users.noreply.github.com>
2022-02-15 16:07:32 -0500

    Update hello world.java

--- hello world.java
+++ hello world.java
@@ -2,6 +2,6 @@ public class hello
 {
     public static void main(string args[]
     {
-        System.out.println( "Hello, " + argv[0] + "!" );
+        System.out.println( "Hello CSCI 4485" + argv[0] + "!" );
     }
 }
